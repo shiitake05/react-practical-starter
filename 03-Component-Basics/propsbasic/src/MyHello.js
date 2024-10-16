d2aa6f4ae639b0dbb67c22e3c75a84e80b9351b2
shiitake05 <pegtar377512@gmail.com>
2024-10-16 22:11:21 +0900

    initial commit

---
+++ 03-Component-Basics/propsbasic/src/MyHello.js
@@ -0,0 +1,23 @@
+// export default function MyHello(props) {
+//   return (
+//     <div>こんにちは、{props.myName}さん！</div>
+//   );
+// }
+
+export default function MyHello({ myName }) {
+  return (
+    <div>こんにちは、{myName}さん！</div>
+  );
+}
+
+// function MyHello(props) {
+//   return (
+//     <div>こんにちは、{props.myName}さん！</div>
+//   );
+// }
+
+// MyHello.propTypes = {
+//   myName: PropTypes.string.isRequired
+// };
+
+// export default MyHello;
