c68c148ccca0d77aa53047cb72bd568b88991d75
shiitake <pegtar377512@gmail.com>
2024-10-22 22:37:12 +0900

    add: comment

--- 03-Component-Basics/statebasic/src/StateBasic.js
+++ 03-Component-Basics/statebasic/src/StateBasic.js
@@ -5,15 +5,18 @@ export default function StateBasic({ init }) {
   console.log(`count is ${count}.`);  // stateではクリックのたびにコンソールのログが追加される。これは関数コンポーネントが再実行されていることを示す。
   // const handleClick = () => setCount(count + 1);
 
-  const handleClick = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-  };
-
+  // setCountの呼び出しの回数にかかわらず、1つずづしか増えない
+  // Stateの更新は非同期で行われるため、setCountが呼び出された時点でのcountの値が使われる
   // const handleClick = () => {
-  //   setCount(c => c + 1);
-  //   setCount(c => c + 1);
+  //   setCount(count + 1);
+  //   setCount(count + 1);
   // };
+
+  // 以下のように記述することで上記の問題を解決できる
+  const handleClick = () => {
+    setCount(b => b + 1);
+    setCount(b => b + 1);
+  };
   return (
     <>
       <button onClick={handleClick}>カウント</button>
